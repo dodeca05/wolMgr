18d3bccc3440712aa2687470838be6697e975029
yu-teacher <yusm1231@gmail.com>
2023-03-18 22:06:57 +0900

    first commit

---
+++ src/main/java/com/pro/WOLmgr/WoLmgrApplication.java
@@ -0,0 +1,13 @@
+package com.pro.WOLmgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoLmgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoLmgrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pro/WOLmgr/WoLmgrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.WOLmgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoLmgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
