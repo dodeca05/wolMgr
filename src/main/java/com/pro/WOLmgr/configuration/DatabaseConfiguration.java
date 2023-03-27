64fb47b59c487d5e69c7885566e30a3815aacb0f
jaehyekim <jaehyekim@ebaykorea.com>
2023-03-28 00:22:45 +0900

    DB configuration yml에서 분리 후 별도 설정으로 추가

---
+++ src/main/java/com/pro/WOLmgr/configuration/DatabaseConfiguration.java
@@ -0,0 +1,28 @@
+package com.pro.WOLmgr.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource("classpath:application.yml")
+public class DatabaseConfiguration {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("db.driverClassName"));
+        dataSource.setUrl(env.getProperty("db.url"));
+        dataSource.setUsername(env.getProperty("db.username"));
+        dataSource.setPassword(env.getProperty("db.password"));
+        return dataSource;
+    }
+}
