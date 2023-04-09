04a33437b59c8424d4f6f14233272b8fb9e68d87
yu-teacher <yusm1231@gmail.com>
2023-04-09 12:47:38 +0900

    import 정리

--- src/main/java/com/pro/WOLmgr/WoLmgrApplication.java
+++ src/main/java/com/pro/WOLmgr/WoLmgrApplication.java
@@ -1,6 +1,5 @@
 package com.pro.WOLmgr;
 
-import com.pro.WOLmgr.util.ExeTimer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/pro/WOLmgr/controller/WolServiceController.java
+++ src/main/java/com/pro/WOLmgr/controller/WolServiceController.java
@@ -1,7 +1,6 @@
 package com.pro.WOLmgr.controller;
 
 import com.pro.WOLmgr.dto.DeviceInfoDTO;
-import com.pro.WOLmgr.util.ExeTimer;
 import org.springframework.web.bind.annotation.*;
 import org.apache.commons.lang3.NotImplementedException;
 
