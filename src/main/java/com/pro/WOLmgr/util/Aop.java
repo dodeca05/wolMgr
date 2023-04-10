de1583b16d18f53047f5b768b1a9805b8523e5c8
yu-teacher <yusm1231@gmail.com>
2023-04-10 14:26:52 +0900

    AOP pointCut 수정

--- src/main/java/com/pro/WOLmgr/util/Aop.java
+++ src/main/java/com/pro/WOLmgr/util/Aop.java
@@ -1,9 +1,11 @@
 package com.pro.WOLmgr.util;
 
 import lombok.extern.log4j.Log4j2;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
@@ -14,14 +16,15 @@ import org.springframework.util.StopWatch;
 @Log4j2
 public class Aop {
 
-    //프로젝트 전체에 AOP를 띄운건데 향후 Service같은 것들이 추가되면 제한합니다.
-    @Pointcut("execution(* com.pro.WOLmgr..*.*(..))")
+    // service 패키지
+    @Pointcut("execution(* com.pro.WOLmgr.service.*.*(..))")
     private void pointCut(){}
 
+    // @ExeTimer 어노테이션이 달렸을 때
     @Pointcut("@annotation(com.pro.WOLmgr.util.ExeTimer)")
     private void timer(){}
 
-    // 시간을 측정하고 싶은 메서드 위에 @ExTimer 어노테이션을 사용하면 메서드의 실행 시간을 측정 가능합니다.
+    // 시간을 측정할 수 있음.
     @Around("timer()")
     public void AssumeExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
 
@@ -37,5 +40,13 @@ public class Aop {
         String methodName = signature.getMethod().getName();
 
         log.info("실행 메서드: {}, 실행시간 = {}ms", methodName, totalTimeMillis);
+    }
+
+    // 클래스 위치와 메서드 위치를 띄워줌
+    @Before("pointCut()")
+    public void logMethodExecution(JoinPoint joinPoint) {
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+        String methodName = joinPoint.getSignature().getName();
+        log.info("[{}] 클래스의 {} 메서드 실행 시작됨", className, methodName);
     }
 }
