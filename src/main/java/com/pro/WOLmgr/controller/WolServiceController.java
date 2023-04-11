d56ec9e05c55609ef151b3ffb3cecf58cd5b5f17
yu-teacher <yusm1231@gmail.com>
2023-04-11 14:27:35 +0900

    제이슨 형태로 반환한다면 HashMap의 형태가 더욱 적합할 것 같아 수정합니다.

--- src/main/java/com/pro/WOLmgr/controller/WolServiceController.java
+++ src/main/java/com/pro/WOLmgr/controller/WolServiceController.java
@@ -4,42 +4,49 @@ import com.pro.WOLmgr.dto.DeviceInfoDTO;
 import org.springframework.web.bind.annotation.*;
 import org.apache.commons.lang3.NotImplementedException;
 
+import java.util.HashMap;
+
 @RestController
 public class WolServiceController{
 
     @PutMapping("/sendPacket/{packet}")
-    public String sendPacket(@PathVariable String packet) {
+    public @ResponseBody HashMap<String,String> sendPacket(@PathVariable String packet) {
+        HashMap<String,String> result = new HashMap<>();
         // TODO: 패킷을 전송하는 로직 구현
         new NotImplementedException("구현이 되지 않은 서비스입니다.");
-        return "{ 'key' : 'value' }";
+        return result;
     }
 
     @GetMapping("/healthCheck/{com}")
-    public String healthCheck(@PathVariable String com){
+    public @ResponseBody HashMap<String,String> healthCheck(@PathVariable String com){
+        HashMap<String,String> result = new HashMap<>();
         // TODO: healthCheck 로직 구현
         new NotImplementedException("구현이 되지 않은 서비스입니다.");
-        return "{ 'key' : 'value' }";
+        return result;
     }
 
     @PostMapping("/computerInfoRegister")
-    public String computerInfo(@RequestBody DeviceInfoDTO deviceInfoDTO){
+    public @ResponseBody HashMap<String,String> computerInfo(@RequestBody DeviceInfoDTO deviceInfoDTO){
+        HashMap<String,String> result = new HashMap<>();
         // TODO: 새로운 디바이스를 등록하는 로직을 추가합니다.
         new NotImplementedException("구현이 되지 않은 서비스입니다.");
-        return deviceInfoDTO.getDevice();
+        return result;
     }
 
     @DeleteMapping("/deleteNum/{num}")
-    public String deleteNum(@PathVariable Long num){
+    public @ResponseBody HashMap<String,String> deleteNum(@PathVariable Long num){
+        HashMap<String,String> result = new HashMap<>();
         // TODO: 등록된 디바이스를 삭제하는 로직을 작성합니다.
         new NotImplementedException("구현이 되지 않은 서비스입니다.");
-        return "{ 'key' : 'value' }";
+        return result;
     }
 
     @PostMapping("/access/{deviceNum}")
-    public String access(@PathVariable Long deviceNum){
+    public @ResponseBody HashMap<String,String> access(@PathVariable Long deviceNum){
+        HashMap<String,String> result = new HashMap<>();
         // TODO: 회원에게 권한을 제공하는 로직을 작성합니다.
         new NotImplementedException("구현이 되지 않은 서비스입니다.");
-        return "{ 'key' : 'value' }";
+        return result;
     }
 
 }
