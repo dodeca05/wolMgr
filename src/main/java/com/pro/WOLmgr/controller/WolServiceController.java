b6ef11e56d2d8f360092adc27ef06ded1ddbed8d
jaehyekim <jaehyekim@ebaykorea.com>
2023-04-25 23:29:54 +0900

    NotImplementedException

--- src/main/java/com/pro/WOLmgr/controller/WolServiceController.java
+++ src/main/java/com/pro/WOLmgr/controller/WolServiceController.java
@@ -19,15 +19,15 @@ public class WolServiceController{
     @PutMapping("/packet/{packet}")
     public String sendPacket(@PathVariable String packet) {
         // TODO: 패킷을 전송하는 로직 구현
-        new NotImplementedException("구현이 되지 않은 서비스입니다.");
-        return "{ 'key' : 'value' }";
+        throw new NotImplementedException("구현이 되지 않은 서비스입니다.");
+        //return "{ 'key' : 'value' }";
     }
 
     @GetMapping("/healthCheck/{com}")
     public String healthCheck(@PathVariable String com){
         // TODO: healthCheck 로직 구현
-        new NotImplementedException("구현이 되지 않은 서비스입니다.");
-        return "{ 'key' : 'value' }";
+        throw new NotImplementedException("구현이 되지 않은 서비스입니다.");
+        //return "{ 'key' : 'value' }";
     }
 
     @PostMapping("/device")
@@ -52,8 +52,8 @@ public class WolServiceController{
     @PostMapping("/access/{deviceNum}")
     public String access(@PathVariable Long deviceNum){
         // TODO: 회원에게 권한을 제공하는 로직을 작성합니다.
-        new NotImplementedException("구현이 되지 않은 서비스입니다.");
-        return "{ 'key' : 'value' }";
+        throw new NotImplementedException("구현이 되지 않은 서비스입니다.");
+        //return "{ 'key' : 'value' }";
     }
 
 }
