ddb9b6dee507b029399ab95ac4f78a4652fae8eb
yu-teacher <yusm1231@gmail.com>
2023-04-28 22:52:32 +0900

    제네릭을 배재해야 할 부분을 찾아 최대한 배재 하였습니다. 로그인 부분 손대봤습니다.

---
+++ src/main/java/com/pro/WOLmgr/handler/ControllerExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.pro.WOLmgr.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred: " + ex.getMessage());
+    }
+
+}
