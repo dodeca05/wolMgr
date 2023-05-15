b0d848192dc8e2d1427c22bc48e33607bb1976e1
jaehyekim <jaehyekim@ebaykorea.com>
2023-05-15 23:22:38 +0900

    웹 토큰 확인 추가

--- src/main/resources/static/js/commonAuth.js
+++ src/main/resources/static/js/commonAuth.js
@@ -2,9 +2,20 @@ function authCheck() {
 
     let Authorization = localStorage.getItem("Authorization");
 
-    let data = { Authorization }
 
-    ajaxForm('POST', '/auth/duplication', data);
+    $.ajax({
+        type: 'GET',
+        url: '/auth/check',
+        dataType: 'JSON',
+        contentType: 'application/json; charset=UTF-8',
+        headers: {
+            'Authorization': Authorization
+        },
+        success: function (result){},
+        error: function (request, status, error) {
+            window.location.href = '/'
+        }
+    });
 
 }
 
@@ -12,19 +23,7 @@ function logout() {
     localStorage.setItem("Authorization", "");
 }
 
-function ajaxForm(type, url, data, successCallback) {
-    $.ajax({
-        type: type,
-        url: url,
-        dataType: 'JSON',
-        contentType: 'application/json; charset=UTF-8',
-        data: JSON.stringify(data),
-        success: successCallback,
-        error: function (request, status, error) {
-            console.log(error)
-        }
-    });
-}
+
 
 $(document).ready(function () {
     authCheck();
